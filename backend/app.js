90f1f934e340f9a6b27e0b7696d462e07b580b66
Alex <websivustot@gmail.com>
2019-10-14 08:42:39 +0300

    mongobd installed

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose");
+
+let app = express();
+app.use(bodyParser.json());
+
+//User databases
+
+app.use(express.static(__dirname+"/static"));
+
+mongoose.connect("mongodb://localhost/shoppinglistdatabase").then(
+	() => {console.log("Connection to mongoDB successful")},
+	(error) => {console.log("Connection to mongoDB failed:"+error)}
+);
+
+const port = process.env.PORT || 3001;
+
+app.listen(port);
+console.log("Running in port 3001")
