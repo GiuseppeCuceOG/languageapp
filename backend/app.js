50fc385805c9b71121eed7ff8c6969a8bd715fa7
Alex <websivustot@gmail.com>
2019-10-30 13:00:11 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
+const session = require("express-session");
+//MongoDB session store for Connect and Express
+const MongoStore = require("connect-mongo")(session);
 
 let app = express();
 app.use(bodyParser.json());
@@ -14,7 +17,20 @@ mongoose.connect("mongodb://localhost/languages").then(
 	(error) => {console.log("Connection to mongoDB failed:"+error)}
 );
 
+app.use(session({
+	name:"languageapp",
+	resave:false,
+	secret:"myBestSecret",
+	saveUninitialized:false,
+	cookie:{maxAge:1000*60*60*24},
+	store: new MongoStore({
+			collection:"session",
+			url:"mongodb://localhost/languages",
+			ttl:24*60*60
+	})
+}));
+
 const port = process.env.PORT || 3001;
 
 app.listen(port);
-console.log("Running in port 3001")
+console.log("Running in port 3001");
